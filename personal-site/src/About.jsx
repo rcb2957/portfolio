840976797b367dd4a7bab408ff878162a8169a40
Riley Brotz <rcb2957@rit.edu>
2026-05-28 02:21:00 -0400

    Added Frameworks, Libraries, and Skills

--- personal-site/src/About.jsx
+++ personal-site/src/About.jsx
@@ -18,23 +18,52 @@ function About() {
                 <li>XML</li>
                 <li>HTML5</li>
                 <li>CSS3</li>
+                <li>PHP</li>
+                <li>C</li>
             </ul>
 
-            <h2></h2>
+            <h2>Libraries</h2>
+            <ul>
+                <li>React</li>
+                <li>jQuery</li>
+            </ul>
+
+            <h2>Frameworks</h2>
+            <ul>
+                <li>Bootstrap</li>
+                <li>Angular</li>
+                <li>JUnit</li>
+                <li>pytest</li>
+                <li>Node.js</li>
+                <li>Spring</li>
+                <li>Selenium</li>
+            </ul>
 
             <h2>Development Tools I Have Used</h2>
             <ul>
                 <li>GitHub</li>
                 <li>Figma</li>
                 <li>Amazon Web Services</li>
+                <li>Powershell</li>
+                <li>Git Bash</li>
                 <li>Visual Studio Code</li>
                 <li>IntelliJ IDEA</li>
                 <li>Docker</li>
                 <li>Jira</li>
                 <li>Postman</li>
             </ul>
-            
-           
+
+            <h2>Skills</h2>
+            <ul>
+                <li>RESTful APIs</li>
+                <li>Object Oriented Programming</li>
+                <li>Software Development Lifecycle</li>
+                <li>Software Testing</li>
+                <li>Web Development</li>
+                <li>Unit Testing</li>
+                <li>Scrum</li>
+                <li>Agile Development</li>
+            </ul>
         </>
     );
 }
