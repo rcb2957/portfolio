38803acfbe571a479431e825e8fe088c9638b58b
Riley Brotz <rcb2957@rit.edu>
2026-05-29 01:28:31 -0400

    Removed unnecessary code with the Vite and React logos

--- personal-site/src/App.jsx
+++ personal-site/src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
 import Home from './Home.jsx'
 import About from './About.jsx'
 import Contact from './Contact.jsx'
@@ -9,15 +7,15 @@ import Work from './Work.jsx'
 import './App.css'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
-function App() {
-  const [count, setCount] = useState(0)
+function toggleTheme() {
+  document.body.classList.toggle("light-mode");
+}
 
-  function toggleTheme() {
-    document.body.classList.toggle("light-mode");
-  }
+function App() {
 
   return (
     <>
+
       {/* Link to Download Resume */}
       <a href="/resume.pdf" download>
         Download Resume
@@ -51,25 +49,7 @@ function App() {
           </Routes>
         </BrowserRouter>
 
-        {/* <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a> */}
       </div>
-      {/* <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
     </>
   );
 }
