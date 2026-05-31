cd796576b2b286113962a20241261d64f787ffa9
Riley Brotz <rcb2957@rit.edu>
2026-05-31 04:25:16 -0400

    Adjusted we view of About page to use flexbox to better spread data across the oage

--- personal-site/src/About.jsx
+++ personal-site/src/About.jsx
@@ -2,92 +2,100 @@ import './App.css'
 
 function About() {
 
-    return (
-        <>
-            {/* About Me Description */}
-            <h2>About Me</h2>
-            <p></p>
+    return (  
+        <div className="grid">
 
-            {/* Education Section */}
-            <h2>Education</h2>
-            <h3>Rochester Institute of Technology</h3>
-            <p>January 2021 - May 2024</p>
-            <ul>
-                <li>Bachelor of Science</li>
-                <li>Major in Software Engineering</li>
-                <li>Minor in Web Development</li>
-                <li>Graduated Cum Laude</li>
-            </ul>
+            <div className="section">
 
-            {/* Certifications Section */}
-            <h2>Certifications</h2>
-            <p>AWS Certified Cloud Practitioner</p>
-            <p>May 2026</p>
+                {/* About Me Description */}
+                <h2>About Me</h2>
+                <p></p>
 
-            {/* Programming Languages HTML */}
-            <h2>Languages I Work With</h2>
-            <ul>
-                <li>Java</li>
-                <li>PostgreSQL</li>
-                <li>MySQL</li>
-                <li>Python</li>
-                <li>C#</li>
-                <li>TypeScript</li>
-                <li>JavaScript</li>
-                <li>JSON</li>
-                <li>XML</li>
-                <li>HTML5</li>
-                <li>CSS3</li>
-                <li>PHP</li>
-                <li>C</li>
-            </ul>
+                {/* Education Section */}
+                <h2>Education</h2>
+                <h3>Rochester Institute of Technology</h3>
+                <p>January 2021 - May 2024</p>
+                <ul>
+                    <li>Bachelor of Science</li>
+                    <li>Major in Software Engineering</li>
+                    <li>Minor in Web Development</li>
+                    <li>Graduated Cum Laude</li>
+                </ul>
 
-            {/* Libraries HTML */}
-            <h2>Libraries</h2>
-            <ul>
-                <li>React</li>
-                <li>jQuery</li>
-            </ul>
+                {/* Certifications Section */}
+                <h2>Certifications</h2>
+                <p>AWS Certified Cloud Practitioner (May 2026)</p>
 
-            {/* Frameworks HTML */}
-            <h2>Frameworks</h2>
-            <ul>
-                <li>Bootstrap</li>
-                <li>Angular</li>
-                <li>JUnit</li>
-                <li>pytest</li>
-                <li>Spring</li>
-                <li>Selenium</li>
-            </ul>
+            </div>
 
-            {/* Development Tools HTML */}
-            <h2>Development Tools</h2>
-            <ul>
-                <li>GitHub</li>
-                <li>Figma</li>
-                <li>Amazon Web Services</li>
-                <li>Powershell</li>
-                <li>Git Bash</li>
-                <li>Visual Studio Code</li>
-                <li>IntelliJ IDEA</li>
-                <li>Docker</li>
-                <li>Jira</li>
-                <li>Postman</li>
-            </ul>
+            <div className = "section">
 
-            {/* Other Skills HTML */}
-            <h2>Skills</h2>
-            <ul>
-                <li>RESTful APIs</li>
-                <li>Object Oriented Programming</li>
-                <li>Software Development Lifecycle</li>
-                <li>Software Testing</li>
-                <li>Web Development</li>
-                <li>Unit Testing</li>
-                <li>Scrum</li>
-                <li>Agile Development</li>
-            </ul>
-        </>
+                {/* Programming Languages HTML */}
+                <h2>Languages I Work With</h2>
+                <ul>
+                    <li>Java</li>
+                    <li>PostgreSQL</li>
+                    <li>MySQL</li>
+                    <li>Python</li>
+                    <li>C#</li>
+                    <li>TypeScript</li>
+                    <li>JavaScript</li>
+                    <li>JSON</li>
+                    <li>XML</li>
+                    <li>HTML5</li>
+                    <li>CSS3</li>
+                    <li>PHP</li>
+                    <li>C</li>
+                </ul>
+
+                {/* Libraries HTML */}
+                <h2>Libraries</h2>
+                <ul>
+                    <li>React</li>
+                    <li>jQuery</li>
+                </ul>
+
+                {/* Frameworks HTML */}
+                <h2>Frameworks</h2>
+                <ul>
+                    <li>Bootstrap</li>
+                    <li>Angular</li>
+                    <li>JUnit</li>
+                    <li>pytest</li>
+                    <li>Spring</li>
+                    <li>Selenium</li>
+                </ul>
+
+                {/* Development Tools HTML */}
+                <h2>Development Tools</h2>
+                <ul>
+                    <li>GitHub</li>
+                    <li>Figma</li>
+                    <li>Amazon Web Services</li>
+                    <li>Powershell</li>
+                    <li>Git Bash</li>
+                    <li>Visual Studio Code</li>
+                    <li>IntelliJ IDEA</li>
+                    <li>Docker</li>
+                    <li>Jira</li>
+                    <li>Postman</li>
+                </ul>
+
+                {/* Other Skills HTML */}
+                <h2>Skills</h2>
+                <ul>
+                    <li>RESTful APIs</li>
+                    <li>Object Oriented Programming</li>
+                    <li>Software Development Lifecycle</li>
+                    <li>Software Testing</li>
+                    <li>Web Development</li>
+                    <li>Unit Testing</li>
+                    <li>Scrum</li>
+                    <li>Agile Development</li>
+                </ul>
+                
+            </div>
+        </div>
     );
 }
 
