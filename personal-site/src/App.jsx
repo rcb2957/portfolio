d4b5ee28bb0f32bfd85023865b1ad56b59eba9a9
Riley Brotz <rcb2957@rit.edu>
2026-06-09 00:49:25 -0400

    Added space between Toggle Theme and Download Resume Buttons

--- personal-site/src/App.jsx
+++ personal-site/src/App.jsx
@@ -32,38 +32,46 @@ function App() {
 
         
           <BrowserRouter>
-        <div className="nav-grid">
-          <div>
-            {/* Links to the different pages of the site */}
-            <nav>
-              <Link to="/">Home</Link> |{" "}
-              <Link to="/about">About</Link> |{" "}
-              <Link to="/projects">Projects</Link> |{" "}
-              <Link to="/work">Work Experience</Link> |{" "}
-              <Link to="/contact">Contact</Link>
-            </nav>
-            </div>
-            {/* Light VS Dark Mode Toggle */}
-            <div>
-            <button onClick={toggleTheme}>
-              Toggle Theme
-            </button>
-            {/* Link to Download Resume */}
-            <a href="/resume.pdf" download>
-              Download Resume
-            </a>
-            </div>
-            </div>
-            <hr />
 
-            {/* Switch between the different pages of the site */}
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/work" element={<Work />} />
-              <Route path="/projects" element={<Projects />} />
-              <Route path="/contact" element={<Contact />} />
-            </Routes>
+              <div className="nav-grid">
+
+                  <div>
+                      {/* Links to the different pages of the site */}
+                      <nav>
+                          <Link to="/">Home</Link> |{" "}
+                          <Link to="/about">About</Link> |{" "}
+                          <Link to="/projects">Projects</Link> |{" "}
+                          <Link to="/work">Work Experience</Link> |{" "}
+                          <Link to="/contact">Contact</Link>
+                      </nav>
+                  </div>
+
+                  
+                  <div className = "nav-buttons">
+
+                      {/* Light VS Dark Mode Toggle */}
+                      <button onClick={toggleTheme}>
+                          Toggle Theme
+                      </button>
+
+                      {/* Link to Download Resume */}
+                      <a href="/resume.pdf" download>
+                          Download Resume
+                      </a>
+
+                  </div>
+
+              </div>
+              <hr />
+
+              {/* Switch between the different pages of the site */}
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/work" element={<Work />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/contact" element={<Contact />} />
+              </Routes>
 
           </BrowserRouter>
 
